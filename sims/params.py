f5a7a550f1d19fe140f7134170ac62b12c255d46
jh1889 <jon.hill@york.ac.uk>
2022-08-29 10:02:15 +0100

    stating with a solid block model

--- sims/params.py
+++ sims/params.py
@@ -1,5 +1,3 @@
-import datetime
-
 # path relative to the root dir of this sim. Leave as mesh/blah.msh in most cases
 mesh_file = 'mesh/my_mesh.msh'
 forcing_boundary = 666
@@ -7,10 +5,6 @@ utm_zone = 56
 utm_band="K"
 cent_lat = -24.15
 cent_lon = 151.8
-spin_up = 432000 # 5 days
-end_time = 3456000 # 40 days
+end_time = 54000 # 15 hours
 output_dir = "output"
-output_time = 900
-constituents = ['M2', 'S2', 'N2', 'K2', 'K1', 'O1', 'P1', 'Q1', 'M4', 'MS4', 'MN4' ]
-# year, month, day, hour, min, sec
-start_datetime = datetime.datetime(2005,11,11,0,0,0) 
+output_time = 60 # 1 minute
